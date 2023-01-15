d08dfe25d83b27a426a83843faa14c9a09558958
silpee <silvimutia7@gmail.com>
2023-01-15 13:58:25 +0700

    finish:)

--- app/src/main/java/com/if3b/sumateraselatan/MainActivity.java
+++ app/src/main/java/com/if3b/sumateraselatan/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Kabupaten & Kota Sumatera Selatan");
 
         rvKabupaten = findViewById(R.id.rv_kabupaten);
         rvKabupaten.setHasFixedSize(true);
